4653a2558221357b3ebf832e6526a3bab3c996c7
brislov <markusbrislov@gmail.com>
2022-01-27 00:43:12 +0100

    first commit

---
+++ MauiPlayground/ViewModels/CountViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Input;
+
+namespace MauiPlayground.ViewModels
+{
+    internal class CountViewModel : BindableObject
+    {
+        private int count;
+        private string displayCount = "Click the button!";
+        public string DisplayCount
+        {
+            get => displayCount;
+            set
+            {
+                displayCount = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand IncreaseCount { get; }
+
+        public CountViewModel()
+        {
+            IncreaseCount = new Command(OnIncrease);
+        }
+
+        void OnIncrease()
+        {
+            count++;
+            DisplayCount = $"Current count: {count}";
+        }
+    }
+}
